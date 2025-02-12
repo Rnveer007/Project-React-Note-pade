2bb5c23120b403830e7ab3fb9c352cf8fdcc5b1e
Rnveer007 <rvs523474@gmail.com>
2025-02-12 17:54:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
+import React from 'react'
+import './index.css'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textAreaInput, setTextAreaInput] = useState('');
+  const [notes, setNotes] = useState([]);
+  console.log(notes)
+  const [colorInput, setColorInput] = useState('');
+
+
+
+  function addNote(e) {
+    e.preventDefault();
+    const obj = { id: Date.now(), note: textAreaInput, color: colorInput }
+    setNotes([...notes, obj])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div >
+        <h1>Note-Pad</h1>
+        <div className='flex'>
+
+          <form onSubmit={addNote} className='border-2 p-3 h-[700px] w-[800px]'>
+            <textarea name="" id="" placeholder='Add Your Notes' value={textAreaInput} onChange={(e) => setTextAreaInput(e.target.value)} className='border-2' required  ></textarea>
+            <input type="color" name="" id="" value={colorInput} onChange={(e) => setColorInput(e.target.value)} required />
+            <div>
+              <button type='submit' className='border-2 px-3'>Add</button>
+            </div>
+          </form>
+
+
+          <div className='bg-gray-400 h-[700px] w-[800px]'>
+            {
+              notes.map((item, index) => {
+                return <h1 key={index} style={{ backgroundColor: item.color }} className='h-[80px] w-[200px] pl-3 pt-2 capitalize font-bold'> {item.note}</h1>
+              })
+            }
+
+
+          </div>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
